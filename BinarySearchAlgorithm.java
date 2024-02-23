0125c61a02c60a9c95f863ac868cd516d04f232d
saurabhsingh <saurabh623a@gmail.com>
2024-02-23 20:30:08 +0530

    Day-1

---
+++ BinarySearchAlgorithm.java
@@ -0,0 +1,26 @@
+public class BinarySearchAlgorithm {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int element = 2;
+        System.out.println(binarySearch(nums, element));
+    }
+    static int binarySearch(int[] arr, int element){
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if (element == arr[mid]) {
+                return mid;
+            }
+            else if (element > arr[mid]){
+                start = mid + 1;
+                continue;
+            }
+            else {
+                end = mid - 1;
+                continue;
+            }
+        }
+        return -1;
+    }
+}
